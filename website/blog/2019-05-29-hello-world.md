38972053d73719cba5b23e3c7503db67e1b851fc
Ben Ebsworth <ben.ebsworth@gmail.com>
2020-06-03 08:39:23 +1000

    init skeleton

---
+++ website/blog/2019-05-29-hello-world.md
@@ -0,0 +1,17 @@
+---
+id: hello-world
+title: Hello
+author: Ben Ebsworth 
+author_title: Engineer
+author_url: https://github.com/castlemilk
+author_image_url: https://avatars0.githubusercontent.com/u/7474316?s=460&u=f2594ba51aaf1a6bab70a6be87c585691b052bf5&v=4
+tags: [hello]
+---
+
+Welcome to this blog. This blog is created with [**Docusaurus 2 alpha**](https://v2.docusaurus.io/).
+
+<!--truncate-->
+
+This is a test post.
+
+A whole bunch of other information.
---
+++ website/docusaurus.config.js
@@ -0,0 +1,101 @@
+module.exports = {
+  title: 'Raspernetes',
+  tagline: 'running a k8s on Raspberry 🥧',
+  url: 'https://raspernetes.io',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Raspernetes',
+      logo: {
+        alt: 'Raspernetes Logo',
+        src: 'img/logo.svg',
+      },
+      links: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/raspbernetes',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Guides',
+              to: 'docs/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/raspernetes',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/raspernetes',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/raspernetes',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/raspbernetes',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Raspernetes, Inc. Built with ❤️`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'doc1',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
