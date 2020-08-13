4f03c5a5ff103e97e91fe39b8721f11a3b6befbd
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2020-08-13 12:07:47 +1000

    adding encryption instructions (#30)
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,8 +1,8 @@
 module.exports = {
   someSidebar: {
     "Raspbernetes": ['introduction', 'styleguide', 'contributing'],
-    "Getting Started": ['installation', 'configuration'],
-    "Advanced": ['remote_kubectl_access', 'architecture', 'advanced_installation'],
+    "Getting Started": ['installation'],
+    "Advanced": ['remote_kubectl_access', 'encrpyt_secrets'],
     "Security": ['cis-benchmark', 'kubesec-benchmark']
   },
 };
